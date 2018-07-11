efb43265c0d03bc467ee5503ff0eb7fca53352ee
Michael Cohen <scudette@gmail.com>
2018-07-11 11:37:37 +1000

    Updated API to include a GetResponseChannel().
    GetResponseChannel() is a method which returns a channel over which a
    single part of the result is sent. This allows clients to receive
    multiple JSON results as they are produced.

--- api.go
+++ api.go
@@ -6,6 +6,89 @@ import (
 	"fmt"
 )
 
+// A response from VQL queries.
+type VFilterJsonResult struct {
+	Part    int
+	Columns []string
+	Payload []byte
+}
+
+// Returns a channel over which multi part results are sent.
+func GetResponseChannel(
+	vql *VQL, ctx context.Context, scope *Scope,
+	maxrows int) <-chan *VFilterJsonResult {
+	result_chan := make(chan *VFilterJsonResult)
+
+	go func() {
+		defer close(result_chan)
+
+		part := 0
+		row_chan := vql.Eval(ctx, scope)
+		columns := vql.Columns(scope)
+		rows := []Row{}
+
+		ship_payload := func() {
+			s, err := json.MarshalIndent(rows, "", " ")
+			if err != nil {
+				scope.Log("Unable to serialize: %v",
+					err.Error())
+				return
+			}
+			result_chan <- &VFilterJsonResult{
+				Part:    part,
+				Columns: *columns,
+				Payload: s,
+			}
+
+			rows = []Row{}
+			part += 1
+		}
+		defer ship_payload()
+		for row := range row_chan {
+			// Send the payload.
+			if len(rows) > maxrows {
+				ship_payload()
+			}
+
+			if len(*columns) == 0 {
+				members := scope.GetMembers(row)
+				columns = &members
+			}
+			if len(*columns) == 0 {
+				rows = append(rows, row)
+
+			} else {
+				new_row := NewDict()
+				for _, key := range *columns {
+					value, pres := scope.Associative(row, key)
+					if pres && !IsNil(value) {
+						var cell Any
+						switch t := value.(type) {
+						case Null:
+							cell = nil
+
+						case fmt.Stringer:
+							cell = value
+
+						case []byte:
+							cell = string(t)
+
+						default:
+							// Pass directly to
+							// Json Marshal
+							cell = value
+						}
+						new_row.Set(key, cell)
+					}
+				}
+				rows = append(rows, new_row)
+			}
+		}
+	}()
+
+	return result_chan
+}
+
 // A convenience function to generate JSON output from a VQL query.
 func OutputJSON(vql *VQL, ctx context.Context, scope *Scope) ([]byte, error) {
 	output_chan := vql.Eval(ctx, scope)
@@ -30,7 +113,6 @@ func OutputJSON(vql *VQL, ctx context.Context, scope *Scope) ([]byte, error) {
 						cell = nil
 
 					case fmt.Stringer:
-						//						cell = t.String()
 						cell = value
 
 					case []byte:
